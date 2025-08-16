8cc58efe146dc04ee36478babfdd9ce46ff3d7f1
Rafliarjunapratama <130772818+Rafliarjunapratama@users.noreply.github.com>
2025-08-16 19:39:48 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,7 +5,8 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.redirect("/api/anime");
 });
