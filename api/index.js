e139dcaa0f7ce6b06f750f557c197a696d77b895
Rafliarjunapratama <130772818+Rafliarjunapratama@users.noreply.github.com>
2025-08-26 12:06:39 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -311,7 +311,8 @@ app.get("/api/anime/search", async (req, res) => {
     const firstAnimeEl = $(".anime-list").first();
     const title = firstAnimeEl.find(".anime-title a").text().trim();
     const link = firstAnimeEl.find(".anime-title a").attr("href");
-    const img = firstAnimeEl.find(".anime-img img").attr("src") || firstAnimeEl.find(".anime-img img").attr("data-src");
+    const imgRaw = firstAnimeEl.find(".anime-img img").attr("src") || firstAnimeEl.find(".anime-img img").attr("data-src");
+    const img = imgRaw.replace(/\/thumb\/\d+x\d+\//, "/");
     const sinopsis = firstAnimeEl.find(".sinopsis-anime").text().trim();
     const tipe = firstAnimeEl.find("table tr:nth-child(1) td:nth-child(2) a").text().trim();
     const eps = firstAnimeEl.find("table tr:nth-child(2) td:nth-child(2)").text().trim();
@@ -337,7 +338,8 @@ app.get("/api/anime/search", async (req, res) => {
       const charType = $(el).find(".char-jenis-karakter small").text().trim();
       const seiyuuName = $(el).find(".char-seiyuu-list a").text().trim();
       const seiyuuLink = $(el).find(".char-seiyuu-list a").attr("href");
-      const seiyuuImg = $(el).find(".seiyuu-img img").attr("src") || $(el).find(".seiyuu-img img").attr("data-src");
+      const seiyuuImgRaw = $(el).find(".seiyuu-img img").attr("src") || $(el).find(".seiyuu-img img").attr("data-src");
+      const seiyuuImg = seiyuuImgRaw.replace(/\/thumb\/\d+x\d+\//, "/");
 
       characters.push({ charName, charLink, charImg, charType, seiyuuName, seiyuuLink, seiyuuImg });
     });
